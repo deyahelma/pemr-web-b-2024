b610fa1d5d515c60675165c244330fbdf97bcdf1
deyahelma <132913937+deyahelma@users.noreply.github.com>
2024-12-10 15:06:14 +0700

    Add files via upload

---
+++ Deyahelma/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+    <style>
+        body{
+            background-color: lightblue;
+        }
+    </style>
+</head>
+<body>
+<?php
+$nama = "deya";
+$a1=6;$a2=3;
+echo $a1 * $a2;
+echo"<br>"; 
+echo $a1 % $a2;
+echo"<br>";
+echo $nama;
+echo "<br><font color='green'> nama saya </font> ".strtoupper($nama); 
+echo "<br>nama saya ".strtolower($nama);
+echo "<br>nama saya $nama";
+echo "<br>Bismilah, seminggu bisa php"
+?>
+</body>
+</html>
